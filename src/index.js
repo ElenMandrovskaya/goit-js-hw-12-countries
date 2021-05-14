8cf6fbd77ca3e8f7f8323ba40ef676459208f408
Elena <e.mandrovska@gmail.com>
2021-05-14 14:45:16 +0300

    added pnotify

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './styles.css';
 import fetchCountries from './js/fetchCountries.js';
 import countriesList from './templates/countriesList.hbs';
 import debounce from 'lodash.debounce';
+import { alert, error, defaultModules } from "@pnotify/core/dist/PNotify";
+import * as PNotifyMobile from "@pnotify/mobile/dist/PNotifyMobile";
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import '../node_modules/@pnotify/core/dist/BrightTheme.css';
+
+defaultModules.set(PNotifyMobile, {});
 
 const input = document.querySelector('.form-control');
 const results = document.querySelector('.results');
@@ -17,7 +23,7 @@ function onSearch(e) {
     fetchCountries(searchQuery)
         .then(renderCounriesList)
         .catch(error => {
-            console.log(error)
+            // console.log(error)
             alert('ERROR')
         })
 }
